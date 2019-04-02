6993ee6b01983c794cf2588c9346ca1393d84090
kaa13051996 <Krupina.Alisa@gmail.com>
2019-04-02 15:45:31 +0700

    Добавление дозаписи

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 from os import listdir
-from os.path import join
+from os.path import join, basename
 from re import sub
 from statistics import mean
 
@@ -10,7 +10,7 @@ class Parser:
         self.__count_records = 10
         self.__records = self.split_list(self.data, self.__count_records)
         self.__features, self.__train, self.__test = self.get_structure()
-        self.per_ones = [row.count('1') / len(row) for row in self.__features]
+        self.mean_ones = [row.count('1') / len(row) for row in self.__features]
         self.mean_train = [mean(row) for row in self.__train]
         self.mean_test = [mean(row) for row in self.__test]
 
@@ -29,10 +29,13 @@ class Parser:
 
     def get_structure(self):
         '''
+        Конвертирует: string (123,123) -> float (123.123). Уберает подстроки: ' ', '\n', 'Обуч: ', 'Тест: '.
+        Структура:
         0 - признаки;
         1,3 - обучение;
         2,4 - тестирование.
-        :return: признаки, обучение, тестирование.
+        :return: признаки [__count_records], обучение [__count_records*2], тестирование [__count_records*2].
+
         '''
         features = [sub(r'[ \n]', '', row) for row in [record[0] for record in self.__records]]
         train = self.split_list([float(elem.replace(',', '.')) for elem in [sub(r'[Обуч: \n]', '', row) for row in
@@ -43,10 +46,19 @@ class Parser:
                                                                             in [2, 4]]]], self.__count_records)
         return features, train, test
 
+    def write_file(self):
+        sep = ';'
+        data = [basename(self.name), self.mean_ones, self.mean_train, self.mean_test]
+        with open('statistics.csv', 'a') as file:
+            for item in data:
+                file.write(str(item) + sep)
+            file.write('\n')
+
 
 if __name__ == '__main__':
     DIR_DATA = r'data\SSO+SSD'
     list_files = listdir(DIR_DATA)
     full_files = [join(DIR_DATA, file_name) for file_name in list_files]
     obj = Parser(full_files[0])
-    print('Win!')
+    obj.write_file()
+    print('Win!!!')
